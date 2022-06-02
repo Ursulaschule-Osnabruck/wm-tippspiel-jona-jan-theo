029a448c8d32659d0fc724a55da858748a1717b2
Jan Gemmer <j.gemmer@urs-os.de>
2022-06-02 14:41:41 +0200

    .

--- database/migrations/2022_06_02_123303_create_mannschafts_table.php
+++ database/migrations/2022_06_02_123303_create_mannschafts_table.php
@@ -15,6 +15,8 @@ class CreateMannschaftsTable extends Migration
     {
         Schema::create('mannschafts', function (Blueprint $table) {
             $table->id();
+            $table->country()
+
             $table->timestamps();
         });
     }
