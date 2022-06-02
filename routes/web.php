24809ff231e516d5110e0284052035fdbeb53e62
Jonas <jonas.hoetker@urs-os.de>
2022-06-02 14:41:26 +0200

    template integriert

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', function(){
+Route::get('/', function(){
 return view('home');
 });
