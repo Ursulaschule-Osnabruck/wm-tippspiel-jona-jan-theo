116256f9f79c97a1dac24cb6e76ce8658b8bd59a
mcF1tt1 <theofrese@gmail.com>
2022-06-04 17:36:43 +0100

    frontend_v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,44 +46,89 @@
             </thead>
             <tbody>
                 <tr>
-                <td>
-                    <div class="row">
-                        <div class="col-6">
-                            GER
+                    <td>
+                        <div class="row">
+                            <div class="col-6">
+                                GER
+                            </div>
+                            <div class="col-6">
+                                <form>
+                                    <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="GER-Tipp">
+                                </form>
+                            </div>
                         </div>
-                        <div class="col-6">
-                            <form>
-                                <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="GER-Tipp">
-                            </form>
+                    </td>
+                    <td>
+                        <div class="row">
+                            <div class="col-6">
+                                DEN
+                            </div>
+                            <div class="col-6">
+                                <form>
+                                    <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="DEN-Tipp">
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    <div class="row">
-                        <div class="col-6">
-                            DEN
-                        </div>
-                        <div class="col-6">
-                            <form>
-                                <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="DEN-Tipp">
-                            </form>
-                        </div>
-                    </div>
-                </td>
-                <td>21.11.22</td>
+                    </td>
+                    <td>21.11.22</td>
                 </tr>
                 <tr>
-                <th>NED</th>
-                <td>ESP</td>
-                <td>22.11.22</td>
+                    <td>
+                        <div class="row">
+                            <div class="col-6">
+                                ESP
+                            </div>
+                            <div class="col-6">
+                                <form>
+                                    <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="ESP-Tipp">
+                                </form>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="row">
+                            <div class="col-6">
+                                BEL
+                            </div>
+                            <div class="col-6">
+                                <form>
+                                    <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="BEL-Tipp">
+                                </form>
+                            </div>
+                        </div>
+                    </td>
+                    <td>22.11.22</td>
                 </tr>
                 <tr>
-                <th>BRA</th>
-                <td>SWE</td>
-                <td>23.11.22</td>
+                    <td>
+                        <div class="row">
+                            <div class="col-6">
+                                FRA
+                            </div>
+                            <div class="col-6">
+                                <form>
+                                    <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="FRA-Tipp">
+                                </form>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="row">
+                            <div class="col-6">
+                                GEB
+                            </div>
+                            <div class="col-6">
+                                <form>
+                                    <input type="number" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="GEB-Tipp">
+                                </form>
+                            </div>
+                        </div>
+                    </td>
+                    <td>23.11.22</td>
                 </tr>
             </tbody>
             </table>
+            <button type="button" class="btn btn-success">Commit</button>
         </div>
 
     </div>
