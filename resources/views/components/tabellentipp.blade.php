76310370b35e20fd854ee4e32aa21cd1da6feabc
mcF1tt1 <theofrese@gmail.com>
2022-06-30 12:39:14 +0100

    fertig

---
+++ resources/views/components/tabellentipp.blade.php
@@ -0,0 +1,40 @@
+<div>
+    <tr>
+        <td>
+            <div class="row">
+                <div class="col-6">
+                    Senegal
+                </div>
+                
+            </div>
+        </td>
+        <td>
+        <div class="row">
+            <div class="col-6">
+                    <form>
+                        <input type="number" min="0" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="SEN-Tipp">
+                    </form>
+            </div>
+            <div class="col-6">
+                    <form>
+                        <input type="number" min="0" class="form-control" id="tippTeam1" aria-describedby="emailHelp" placeholder="NIE-Tipp">
+                    </form>
+            </div>
+        </div>
+        </td>
+        <td>
+            <i> 0:0</i>
+        </td>
+
+        <td>
+            <div class="row">
+                <div class="col-6">
+                    Niederlande
+                </div>
+
+            </div>
+        </td>
+        <td>21.11.22</td>
+        <td>11:00</td>
+    </tr>
+</div>
